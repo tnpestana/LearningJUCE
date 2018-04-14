6e44539b309c29a38c3ef05b3d8357f713257c19
Tiago Pestana <tnevespestana@gmail.com>
2018-04-14 02:05:22 +0100

    update label and textbox size for reverb components

--- MaxSynth/Source/ReverbEditor.cpp
+++ MaxSynth/Source/ReverbEditor.cpp
@@ -15,33 +15,33 @@ ReverbEditor::ReverbEditor(MaxSynthAudioProcessor& p)
 {
 	addAndMakeVisible(dryWetSlider);
 	dryWetSlider.setSliderStyle(Slider::RotaryVerticalDrag);
-	dryWetSlider.setTextBoxStyle(Slider::TextBoxBelow, false, 35, 20);
+	dryWetSlider.setTextBoxStyle(Slider::TextBoxBelow, false, 40, 20);
 	dryWetSlider.setRange(0, 1, 0.01);
 
 	addAndMakeVisible(dryWetLabel);
-	dryWetLabel.setText("Dry/Wet", dontSendNotification);
+	dryWetLabel.setText("dry/Wet", dontSendNotification);
 	dryWetLabel.setJustificationType(Justification::centred);
 	dryWetAttachment = new AudioProcessorValueTreeState::SliderAttachment(p.treeState,
 		"dryWet", dryWetSlider);
 
 	addAndMakeVisible(roomSizeSlider);
 	roomSizeSlider.setSliderStyle(Slider::RotaryVerticalDrag);
-	roomSizeSlider.setTextBoxStyle(Slider::TextBoxBelow, false, 35, 20);
+	roomSizeSlider.setTextBoxStyle(Slider::TextBoxBelow, false, 40, 20);
 	roomSizeSlider.setRange(0, 1, 0.01);
 
 	addAndMakeVisible(roomSizeLabel);
-	roomSizeLabel.setText("Room Size", dontSendNotification);
+	roomSizeLabel.setText("room Size", dontSendNotification);
 	roomSizeLabel.setJustificationType(Justification::centred);
 	roomSizeAttachment = new AudioProcessorValueTreeState::SliderAttachment(p.treeState,
 		"roomSize", roomSizeSlider);
 
 	addAndMakeVisible(dampingSlider);
 	dampingSlider.setSliderStyle(Slider::RotaryVerticalDrag);
-	dampingSlider.setTextBoxStyle(Slider::TextBoxBelow, false, 35, 20);
+	dampingSlider.setTextBoxStyle(Slider::TextBoxBelow, false, 40, 20);
 	dampingSlider.setRange(0, 1, 0.01);
 
 	addAndMakeVisible(dampingLabel);
-	dampingLabel.setText("Damping", dontSendNotification);
+	dampingLabel.setText("damping", dontSendNotification);
 	dampingLabel.setJustificationType(Justification::centred);
 	dampingAttachment = new AudioProcessorValueTreeState::SliderAttachment(p.treeState,
 		"damping", dampingSlider);
@@ -60,7 +60,7 @@ void ReverbEditor::paint(Graphics& g)
 void ReverbEditor::resized()
 {
 	const int labelWidth = getWidth() / 3;
-	const int labelHeight = 25;
+	const int labelHeight = 20;
 	const int sliderWidth = getWidth() / 3;
 	const int sliderHeight = getHeight() - labelHeight;
 
